720f1bb723076fb79a179d196caacb12eeae35bc
Түвшин Эрдэнэбат <24LP0845@G401-CC0091.local>
2024-10-31 11:22:44 +0800

    es5 zassan

--- back-end/server.js
+++ back-end/server.js
@@ -1,17 +1,16 @@
-// import express, { response } from "express";
+import express, { response } from "express";
 
-// const app = express();
-// const port = 8888;
+const app = express();
+const port = 8888;
 
-// app.listen(port, () => {
-//   console.log("server es6 ajilla");
-// });
-
-import http from "http";
-const http = require("http");
-
-const server = http.createServer((request, response) => {
-  console.log(request, "server es5 ajilla");
-  response.end("hello world");
+app.listen(port, () => {
+  console.log("server es6 ajilla");
 });
-server.listen(8000);
+
+// const http = require("http");
+
+// const server = http.createServer((request, response) => {
+//   console.log(request, "server es5 ajilla");
+//   response.end("hello world");
+// });
+// server.listen(8000);
